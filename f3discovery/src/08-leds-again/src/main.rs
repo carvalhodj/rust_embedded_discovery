81581b4a0e9ed0aade526a39f35c7960e5172669
Daniel Carvalho <danieljosecarvalho@gmail.com>
2023-03-09 22:11:55 -0300

    feat: set the right registers to power on the gpioe

--- f3discovery/src/08-leds-again/src/main.rs
+++ f3discovery/src/08-leds-again/src/main.rs
@@ -9,6 +9,18 @@ fn main() -> ! {
     let (gpioe, rcc) = aux8::init();
 
     // TODO initialize GPIOE
+    rcc.ahbenr.write(|f| f.iopeen().set_bit());
+
+    gpioe.moder.write(|w| {
+        w.moder8().bits(1);
+        w.moder9().bits(1);
+        w.moder10().bits(1);
+        w.moder11().bits(1);
+        w.moder12().bits(1);
+        w.moder13().bits(1);
+        w.moder14().bits(1);
+        w.moder15().bits(1)
+    });
 
     // Turn on all the LEDs in the compass
     gpioe.odr.write(|w| {
